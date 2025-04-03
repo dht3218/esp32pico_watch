9e822d6631e72772826474571cb6f9153a28a50e
dht3218 <825540444@qq.com>
2025-04-03 21:16:01 +0800

    add another theme for UI_Anya

---
+++ another theme for UI_Anya/dht8-3-1_events.py
@@ -0,0 +1,80 @@
+
+def setBLight(event_struct):
+    return
+
+
+def SettingStart(event_struct):
+    return
+
+
+def Wifistart(event_struct):
+    return
+
+
+def getTWifi(event_struct):
+    return
+
+
+def SetClock(event_struct):
+    return
+
+
+def setHour0(event_struct):
+    return
+
+
+def setHour1(event_struct):
+    return
+
+
+def setMinu0(event_struct):
+    return
+
+
+def setMinu1(event_struct):
+    return
+
+
+def ChatClick(event_struct):
+    return
+
+
+def gotuofunc(event_struct):
+    return
+
+
+def setlightU(event_struct):
+    return
+
+
+def setlightD(event_struct):
+    return
+
+
+def changereminder(event_struct):
+    return
+
+
+def ALARM1cfg(event_struct):
+    return
+
+
+def ALARM1set(event_struct):
+    return
+
+
+def ALARM2cfg(event_struct):
+    return
+
+
+def ALARM2set(event_struct):
+    return
+
+
+def ALARM3cfg(event_struct):
+    return
+
+
+def ALARM3set(event_struct):
+    return
+
